75cb48cc5041dd62c342d26fc61ed467fe2b89d7
brunonepomuceno <brunobezerranepomuceno@gmail.com>
2025-02-15 20:09:43 -0300

    translation of the comments and adjust in dryThreshold

--- plant_irrigation.ino
+++ plant_irrigation.ino
@@ -7,22 +7,22 @@ void setup() {
   pinMode(pumpPin, OUTPUT);
   digitalWrite(pumpPin, HIGH); // A bomba precisa estar desligada no inicio
   Serial.begin(9600);
-  Serial.println("Plant Irrigation System Initialized!");
+  Serial.println("Sistema de irrigação de plantas inicializado!");
 }
 
 void loop() {
   int moistureLevel = analogRead(moistureSensorPin); //Ler a umidade do solo
-  Serial.print("Soil Moisture Level: ");
+  Serial.print("Nível de umidade do solo:");
   Serial.println(moistureLevel);
 
   if (moistureLevel > dryThreshold) {
-    Serial.println("Soil is DRY! Watering the plant...");
-    digitalWrite(pumpPin, LOW);  // Liga a bomba
+    Serial.println("O solo está SECO! Regando a planta...");
+    digitalWrite(pumpPin, HIGH);  // Liga a bomba
     delay(2000);                 // Quantidade de água liberada em segundos
-    digitalWrite(pumpPin, HIGH); // Desliga a bomba
-    Serial.println("Watering complete.");
+    digitalWrite(pumpPin, LOW); // Desliga a bomba
+    Serial.println("Rega concluída.");
   } else {
-    Serial.println("Soil is WET. No watering needed.");
+    Serial.println("O solo está MOLHADO. Não é necessário regar.");
   }
 
   delay(1000); // Delayzin antes a próxima leitura
